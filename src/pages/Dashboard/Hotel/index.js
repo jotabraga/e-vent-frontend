db0b800c740d92b592d7b7c570cc3b8ebb2ca456
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-03 14:34:54 -0300

    feat: title and subtitle on hotel tab

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -14,7 +14,7 @@ export default function Hotel() {
     result.catch(() => {}); //fazer o catch com toastify
   }, []);
   return (
-    <>
+    <Body>
       <h1>Escolha de hotel e quarto</h1>
       <h2>Primeiro, escolha seu hotel</h2>
       <HotelOptions>
@@ -22,10 +22,25 @@ export default function Hotel() {
           <HotelCard key={h.id} hotel={h} />
         ))}
       </HotelOptions>
-    </>
+    </Body>
   );
 }
 const HotelOptions = styled.div`
   display: flex;
   gap: 20px;
+`;
+const Body = styled.div`
+  font-family: "Roboto";
+  & > h1 {
+    color: #000;
+    font-size: 34px;
+    line-height: 40px;
+    margin-bottom: 40px;
+  }
+  & > h2 {
+    color: #8e8e8e;
+    font-size: 20px;
+    line-height: 23px;
+    margin-bottom: 18px;
+  }
 `;
