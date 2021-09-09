313bf0f50b36d5a1c02c092de4a1b7ae4e8aeb96
jotabraga <jpmbraga.feg@gmail.com>
2021-09-09 11:30:50 -0300

    feat: add style to credit card

--- src/components/Payment/StyledCard.js
+++ src/components/Payment/StyledCard.js
@@ -4,6 +4,21 @@ const StyledCard = styled.div`
   form {
     diplay: flex;
     flex-flow: column nowrap;
+    input {
+      width: 100%;
+      height: 40px;
+      padding: 5px;
+      margin: 5px 0;
+      border: 1px solid #e8e8e8;
+      border-radius: 5px;
+      ::placeholder {
+        color: #aeaeae;
+        padding-left: 5px;
+      }
+      ::focus {
+        border-color: #8e8e8e;
+      }
+    }
     .payment {
       display: flex;
       margin: 20px 0;
@@ -23,21 +38,6 @@ const StyledCard = styled.div`
             margin-left: 5px;
           }
         }
-      }
-    }
-    input {
-      width: 100%;
-      height: 40px;
-      padding: 5px;
-      margin: 5px 0;
-      border: 1px solid #e8e8e8;
-      border-radius: 5px;
-      ::placeholder {
-        color: #aeaeae;
-        padding-left: 5px;
-      }
-      ::focus {
-        border-color: #8e8e8e;
       }
     }
   }
