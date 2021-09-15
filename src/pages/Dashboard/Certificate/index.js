f9b8bb520f875757e6478cc5b51c5ce798647675
Daniel Capeans <dcapeans@gmail.com>
2021-09-15 16:08:08 -0300

    feat: certificate page layout

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,3 +1,37 @@
+import { Typography } from "@material-ui/core";
+import styled from "styled-components";
+
 export default function Certificate() {
-  return "Certificado: Em breve!";
+  return (
+    <Body>
+      <StyledTypography variant="h4">Certificado</StyledTypography>
+      <Container>
+        <span>O certificado estará disponível após a conclusão dos eventos</span>
+      </Container>
+    </Body>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+  text-align: left;
+`;
+
+const Body = styled.div`
+  display: block;
+  position: relative;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  color: #8e8e8e;
+  font-size: 20px;
+  font-family: "Roboto";
+`;
