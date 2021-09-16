30ab750584370a29f9659cf74a17ba0b15d0a0cc
Fernando <valanife@gmail.com>
2021-09-16 14:59:21 -0300

    fix: activity message break

--- src/pages/Dashboard/Activities/styles.js
+++ src/pages/Dashboard/Activities/styles.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 const Message = styled.h1`
   font-size: 20px;
   color: #8e8e8e;
-  width: 100%;
+  width: 510px;
   height: auto;
   display: flex;
   justify-content: center;
   text-align: center;
-  margin-top: 200px;
+  margin: 200px auto;
   flex-direction: column;
   display: ${(props) => (props.show ? "block" : "none")};
 `;
