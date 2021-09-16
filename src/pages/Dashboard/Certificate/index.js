b67ad06efbb38754c08c9ed1030696657541ac3b
Daniel Capeans <dcapeans@gmail.com>
2021-09-16 13:43:01 -0300

    feat: certificate image and download as PDF

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,16 +1,70 @@
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
+import { useContext, useEffect } from "react";
+import { PDFDownloadLink, StyleSheet } from "@react-pdf/renderer";
 import styled from "styled-components";
+import EventInfoContext from "../../../contexts/EventInfoContext";
+import CertificateFile from "./CertificateCreator";
+import { useState } from "react";
+import useApi from "../../../hooks/useApi";
 
 export default function Certificate() {
+  const { eventInfo } = useContext(EventInfoContext);
+  const { enrollment } = useApi();
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    enrollment.getPersonalInformations().then(response => {
+      if (response.status !== 200) {
+        return;
+      }
+      const { name } = response.data;
+      setUsername(name);
+    });
+  }, []);
+
   return (
     <Body>
-      <StyledTypography variant="h4">Certificado</StyledTypography>
-      <Container>
-        <span>O certificado estará disponível após a conclusão dos eventos</span>
-      </Container>
+      { eventInfo &&
+        <>
+          <StyledTypography variant="h4">Certificado</StyledTypography>
+          <Container>
+            {dayjs().isAfter(dayjs(eventInfo.endDate)) 
+              ? <CertificateContainer>
+                {/* <CertificateImage>
+                  <CertificateFile eventInfo={eventInfo} username={username}/>
+                </CertificateImage> */}
+                <PDFDownloadLink 
+                  document={<CertificateFile eventInfo={eventInfo} username={username}/>}
+                  fileName="certificado_drivent.pdf"
+                  style={styles.button}
+                  children={
+                    <CertificateImage>
+                      <CertificateFile eventInfo={eventInfo} username={username}/>
+                    </CertificateImage>
+                  }
+                >
+                </PDFDownloadLink>
+              </CertificateContainer> 
+              : <span>O certificado estará disponível após a conclusão dos eventos</span>
+            }  
+          </Container>
+        </>
+      }
     </Body>
   );
 }
+
+const styles = StyleSheet.create({
+  button: { 
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    textDecoration: "none"
+  }
+});
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px !important;
@@ -34,4 +88,19 @@ const Container = styled.div`
   color: #8e8e8e;
   font-size: 20px;
   font-family: "Roboto";
+  margin-top: 15px;
+`;
+
+const CertificateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+  height: 85%;
+`;
+
+const CertificateImage = styled.div`
+  height: 100%;
+  width: 100%;
+  border: 5px solid #000;
 `;
